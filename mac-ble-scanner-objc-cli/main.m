9e334ea41c565e717e4cf9cd1520f632d68dfa62
Itamar Hassin <ihassin@mac.com>
2018-01-14 15:56:43 -0500

    Initial Commit

---
+++ mac-ble-scanner-objc-cli/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  mac-ble-scanner-objc-cli
+//
+//  Created by Itamar Hassin on 1/14/18.
+//  Copyright © 2018 Itamar Hassin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
